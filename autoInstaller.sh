46f8178be3dcf30b78573f6af3be03c3f959b869
Diego Duran <96787803+ArchDiego@users.noreply.github.com>
2022-07-19 18:54:43 -0500

    Add files via upload

---
+++ autoInstaller.sh
@@ -0,0 +1,34 @@
+echo "Ejecutando script"
+echo "Instalando qtile"
+
+# Instalar dependencias
+pacman -S qtile pacman-contrib python python-pip pulseaudio kitty --noconfirm 
+yay -S nerd-fonts-ubuntu-mono --noconfirm
+pip install psutil
+
+cp -r ./kitty/ ~/.config/
+cp -r ./qtile/ ~/.config/
+cp -r ./Wallpapers/ $HOME/Pictures/
+
+
+echo "Instalando bspwm"
+
+sudo pacman -Syu --needed git base-devel
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin && makepkg -si
+paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer dunst rofi ksuperkey nerd-fonts-jetbrains-mono polkit-gnome fm6000 network-manager-applet helix xfce4-power-manager betterlockscreen zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
+git clone https://github.com/theCode-Breaker/bspwm-dotfiles.git --depth 1
+cd bspwm-dotfiles
+cp -R .config/* ~/.config/
+chmod -R +x ~/.config/bspwm
+cp .zshrc ~
+cp .zshrc-personal ~
+mkdir ~/.local/bin
+cp -R .local/bin/* ~/.local/bin
+chmod -R +x ~/.local/bin
+betterlockscreen -u ~/.config/bspwm/backgrounds/evening-sky.png
+sudo systemctl enable betterlockscreen@$USER.service
+
+
+
+
